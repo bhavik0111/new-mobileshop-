56d60305fafa516dc28ea70fa65135b09ab76dd3
bhavik0111 <118987227+bhavik0111@users.noreply.github.com>
2023-02-10 17:59:05 +0530

    Update footer.php

--- mobileshop/footer.php
+++ mobileshop/footer.php
@@ -1,4 +1,14 @@
  <tr><td align="center"><h4>@CopyRight Mobileshop</h4></td></tr>
 </table>
+<script>
+    
+var splide = new Splide( '.splide', {
+  type  : 'fade',
+  rewind: true,
+} );
+
+splide.mount();
+
+</script>
 </body>
 </html>
